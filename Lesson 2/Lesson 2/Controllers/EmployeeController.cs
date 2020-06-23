5e583c6378694d235f992c6c1cefbb65eda6887d
Hector <hectorgatsos@gmail.com>
2020-06-23 22:02:05 +0300

    Telos Ston pinaka employee Fere mou aytous pou exoun monaxa ena project (Filter ana project) Ston pinaka projects fere kane pagination Ston pinaka kane sorting

--- Lesson 2/Lesson 2/Controllers/EmployeeController.cs
+++ Lesson 2/Lesson 2/Controllers/EmployeeController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using PagedList;
 
 namespace Lesson_2.Controllers
 {
@@ -11,12 +13,38 @@ namespace Lesson_2.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult Index(string searchName,int? searchSalaryMin, int? searchSalaryMax, int? page)
         {
-            var employees = db.Employees.ToList();
+            var employees = db.Employees.ToList();  //Fertous olous
 
 
-            return View(employees);
+            
+
+            //Filtering
+            if (!String.IsNullOrWhiteSpace(searchName))  //Filtrare kapoious an exei timi to searchname
+            {
+                employees = employees.Where(x => x.Name.ToUpper().Contains(searchName.ToUpper())).ToList();
+            }
+
+            if (!(searchSalaryMin is null))  //searchSalaryMin!=null  //Filtrare kapoious an exei timi to searchSalaryMin
+            {
+                employees = employees.Where(x => x.Salary >= searchSalaryMin).ToList();
+            }
+
+            if (!(searchSalaryMax is null))  //searchSalaryMin!=null   //Filtrare kapoious an exei timi to searchSalaryMax
+            {
+                employees = employees.Where(x => x.Salary <= searchSalaryMax).ToList();
+            }
+
+            //(1) Fere mou aytous pou exoun monaxa ena project 
+
+
+
+            int pageSize = 4;
+            int pageNumber = page ?? 1;
+
+
+            return View(employees.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult Index2()
